6dc16a51a5738a869596002b62391aa860adddb9
Moises Fuentes <moyxess@gmail.com>
2021-06-20 20:52:45 -0500

    Description added

---
+++ app.js
@@ -0,0 +1,6 @@
+
+// Ahorcado Game v1.0.0
+//Objetivo: Desarrollar el juego del ahorcado y jugar mediante consola para esta primera
+//          versión, más adelante, se deasrrollará el mimso juego pero en un entorno web
+//          con animaciones e imagenes interactivas
+console.log('Game stared');
